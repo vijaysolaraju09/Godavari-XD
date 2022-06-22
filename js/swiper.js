494bfb20795caa452cd59a4bb946e40b7d5caa2b
vijaysolaraju09 <92515032+vijaysolaraju09@users.noreply.github.com>
2022-06-22 17:37:29 +0530

    Delete js directory

--- js/swiper.js
+++
@@ -1,9 +0,0 @@
-$().ready(function(){
-    $('.slick-carousel').slick({
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-      });
-      
-  });
