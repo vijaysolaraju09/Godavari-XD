ab4a38d42875f25b973bbf536359056469c7a0eb
vijaysolaraju09 <92515032+vijaysolaraju09@users.noreply.github.com>
2022-06-22 17:39:52 +0530

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$().ready(function(){
+    $('.slick-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+      
+  });
